becb7852da4153fbf30043dabfd9faca48a33457
Olga Conesa <olgaconesaboj@gmail.com>
2020-10-06 14:01:32 +0200

    Acciones y Reducer básico

--- 01-redux-basic/app-1.ts
+++ 01-redux-basic/app-1.ts
@@ -1,2 +1,28 @@
 
-console.log('Hola Mundo');
+//Crear una accion (dice lo que tiene que hacer)
+interface Action {
+    type: string;
+    payload?: any;
+}
+
+//Para utilizar action creamos una constante
+/* const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+}; */
+
+//Action se usará en un reducer que recibe 2 paramatros(state, action)
+//Reducer debe regresar siempre un estado
+function reducer( state = 10, action: Action ) {
+
+    if (action.type === 'INCREMENTAR') {
+        return state += 1;
+    }
+    
+    return state;
+}
+
+//Usar el reduce
+const incrementadorAction: Action = {
+    type: 'INCREMENTAR'
+};
+console.log(reducer(10, incrementadorAction));
