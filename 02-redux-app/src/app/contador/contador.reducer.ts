9ef5e0a5cfdba1f4b70ef8bf5687bbcba98fd5d0
Olga Conesa <olgaconesaboj@gmail.com>
2020-10-09 10:10:54 +0200

    Creando acción de multiplicar y dividir

--- 02-redux-app/src/app/contador/contador.reducer.ts
+++ 02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,13 +1,15 @@
 import { createReducer, on } from '@ngrx/store';
-import { incrementar, decrementar } from './contador.actions';
+import { incrementar, decrementar, multiplicar, dividir } from './contador.actions';
 
 // Crear estado inicial de la app o del reducer
 export const initialState = 20;
 
 //Crear una función privada con el reducer (es el ActionReducer)
 const _contadorReducer = createReducer(initialState,
-    on(incrementar, (state) => state + 1),
-    on(decrementar, (state) => state - 1),
+    on(incrementar, state => state + 1),
+    on(decrementar, state => state - 1),
+    on(multiplicar, ( state, { numero } ) => state * numero ),
+    on(dividir, ( state, { numero } ) => state / numero)
 );
 
 //crear la funci´-on que estamos exportando, recibe el state y el action
--- 02-redux-app/src/app/contador/hijo/hijo.component.ts
+++ 02-redux-app/src/app/contador/hijo/hijo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../app.reducers';
+import * as actions from '../contador.actions';
 
 
 @Component({
@@ -25,14 +26,12 @@ export class HijoComponent implements OnInit {
 
   multiplicar() {
 
-    /*  this.contador *= 2;
-     this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.multiplicar({numero: 2}));
   }
 
   dividir() {
 
-    /*  this.contador /= 2;
-     this.cambioContador.emit(this.contador); */
+    this.store.dispatch(actions.dividir({numero: 2}));
   }
 
 
