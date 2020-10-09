20e1bbf989c2e3c78dc0a8082fb8d27b101ca5a6
Olga Conesa <olgaconesaboj@gmail.com>
2020-10-09 09:11:01 +0200

    Create Reducer

--- 02-redux-app/src/app/contador/contador.reducer.ts
+++ 02-redux-app/src/app/contador/contador.reducer.ts
@@ -1,19 +1,17 @@
-import { Action } from '@ngrx/store';
+import { createReducer, on } from '@ngrx/store';
 import { incrementar, decrementar } from './contador.actions';
 
+// Crear estado inicial de la app o del reducer
+export const initialState = 20;
 
-export function contadorReducer(state: number = 10, action: Action) {
+//Crear una función privada con el reducer (es el ActionReducer)
+const _contadorReducer = createReducer(initialState,
+    on(incrementar, (state) => state + 1),
+    on(decrementar, (state) => state - 1),
+);
 
-    switch (action.type) {
-
-        case incrementar.type:
-            return state + 1;
-
-        case decrementar.type:
-            return state - 1;
-
-        default:
-            return state;
-
-    }
+//crear la funci´-on que estamos exportando, recibe el state y el action
+//va a utilizar el reducer que creamos.
+export function contadorReducer(state, action) {
+    return _contadorReducer(state, action);
 }
