a61323b8a6c1ff24f323e6bb08ac502ddd10bb75
Olga Conesa <olgaconesaboj@gmail.com>
2020-10-20 13:50:37 +0200

    Crear nuestro primer usuario

--- 04-ingresoEgresoApp/src/app/auth/register/register.component.ts
+++ 04-ingresoEgresoApp/src/app/auth/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -11,23 +13,34 @@ export class RegisterComponent implements OnInit {
 
   registroForm: FormGroup;
 
-  constructor( private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+              private authService: AuthService,
+              private router: Router) { }
 
   ngOnInit(): void {
 
     this.registroForm = this.fb.group({
 
-      nombre:   ['', Validators.required],
-      correo:   ['', [Validators.required, Validators.email]],
+      nombre: ['', Validators.required],
+      correo: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
 
     });
   }
 
-  crearUsuario(){
-    console.log(this.registroForm);
-    console.log(this.registroForm.valid);
-    console.log(this.registroForm.value);
+  crearUsuario() {
+
+    if (this.registroForm.invalid) { return; }
+
+    const { nombre, correo, password } = this.registroForm.value;
+
+    this.authService.crearUsuario(nombre, correo, password)
+      .then(credenciales => {
+        console.log(credenciales);
+        this.router.navigate(['/'])
+      })
+      .catch(err => console.error(err));
+
   }
 
 }
---
+++ 04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(public auth: AngularFireAuth) { }
+
+  crearUsuario( nombre: string, email:string, password: string) {
+
+    //console.log({nombre, email, password})
+
+    return this.auth.createUserWithEmailAndPassword( email, password );
+  }
+}
