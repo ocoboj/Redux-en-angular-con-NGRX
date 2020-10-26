2aab263f112618a77fe04fd344f5abb3d74202cf
Olga Conesa <olgaconesaboj@gmail.com>
2020-10-26 15:37:40 +0100

    Obtener información del usuario activo en Firebase

--- 04-ingresoEgresoApp/src/app/services/auth.service.ts
+++ 04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -8,6 +8,15 @@ export class AuthService {
 
   constructor(public auth: AngularFireAuth) { }
 
+  initAuthListener() {
+    this.auth.authState.subscribe( fuser => {
+      console.log(fuser);
+      console.log(fuser?.uid);
+      console.log(fuser?.email);
+    });
+  }
+
+
   crearUsuario( nombre: string, email:string, password: string) {
 
     //console.log({nombre, email, password})
