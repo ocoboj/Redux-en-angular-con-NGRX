662ef6881807de60b15e80bb4aaacc7979796798
Olga Conesa <olgaconesaboj@gmail.com>
2020-11-08 10:44:23 +0100

    Crear documento de base de datos por cada usuario

--- 04-ingresoEgresoApp/src/app/services/auth.service.ts
+++ 04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
+
+import 'firebase/firestore';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore } from '@angular/fire/firestore';
 import { map } from 'rxjs/operators';
+import { Usuario } from '../models/usuario.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth, public firestore: AngularFirestore) { }
 
   initAuthListener() {
     this.auth.authState.subscribe( fuser => {
@@ -22,7 +26,13 @@ export class AuthService {
 
     //console.log({nombre, email, password})
 
-   return this.auth.createUserWithEmailAndPassword( email, password );
+   return this.auth.createUserWithEmailAndPassword( email, password )
+     .then( ({user}) => {
+
+       const newUser = new Usuario(user.uid, nombre, user.email);
+
+       return this.firestore.doc(`${ user.uid }/usuario`).set({...newUser});
+     });
   }
 
   loginUsuario( email: string, password: string) {
