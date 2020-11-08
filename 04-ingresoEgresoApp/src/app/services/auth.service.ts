e00744c63f0e8f83a4d502b104453c1d43504c28
Olga Conesa <olgaconesaboj@gmail.com>
2020-11-08 10:20:58 +0100

    Guard - Proteger el dashboard y sus rutas hijas

--- 04-ingresoEgresoApp/src/app/services/auth.service.ts
+++ 04-ingresoEgresoApp/src/app/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -30,5 +31,11 @@ export class AuthService {
 
   logout() {
   return this.auth.signOut();
+  }
+
+  isAuth() {
+    return this.auth.authState.pipe(
+      map( fbUser => fbUser != null)
+    );
   }
 }
